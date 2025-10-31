a0a2d390512c248e05a7e7154a54cae5758180fe
ichengwang <411221250@gms.ndhu.edu.tw>
2025-10-31 09:08:47 +0000

    feat: Complete 01-helloRVOS 作業一 - Console polling input and echo

--- os_development/kernel/kernel.c
+++ os_development/kernel/kernel.c
@@ -8,6 +8,11 @@ void start_kernel(void)
 	int i=10;
 	kprintf("i=%d\n",i);
 	while(1) 
-	{}
+	{
+		char c = uart_getc();
+		if(c == 13) uart_putc('\n');
+		else if(c == 127) uart_puts("\b \b");
+		else uart_putc(c);
+	}
 }
 
